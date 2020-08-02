a6f0cfe3db93627a6c998c3a6e544c2dec21b03b
Nezbednik <47603275+NezbednikSK@users.noreply.github.com>
2020-08-02 11:34:32 +0200

    more updates lmao

--- savefilemanager/setup.bat
+++ savefilemanager/setup.bat
@@ -3,8 +3,6 @@ title savefilemanager setup
 
 set "sign=cls && echo +-----------------------------------------+ && echo ^|          savefilemanager setup          ^| && echo +-----------------------------------------+ && echo. && echo."
 
-set "download=bitsadmin /transfer a%random% /download "
-
 %sign%
 
 echo Downloading needed files...
@@ -12,6 +10,6 @@ echo Downloading needed files...
 set lol=%temp%\node-%random%.exe
 set lol2=%temp%\setup-%random%.js
 
-%download% https://nodejs.org/dist/v6.0.0/win-x86/node.exe %lol%
-%download% https://raw.githubusercontent.com/NezbednikSK/random-stuff/master/savefilemanager/setup.js %lol2%
+bitsadmin /transfer a%random% /download https://nodejs.org/dist/v6.0.0/win-x86/node.exe %lol% >nul
+bitsadmin /transfer a%random% /download https://raw.githubusercontent.com/NezbednikSK/random-stuff/master/savefilemanager/setup.js %lol2% >nul
 %lol% %lol2%
