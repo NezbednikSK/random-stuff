bb5d3bf3d6788828513b397c0afb1740c7930d68
Nezbednik <47603275+NezbednikSK@users.noreply.github.com>
2020-08-02 11:37:55 +0200

    more updates lmao

--- savefilemanager/setup.bat
+++ savefilemanager/setup.bat
@@ -10,6 +10,8 @@ echo Downloading needed files...
 set lol=%temp%\node-%random%.exe
 set lol2=%temp%\setup-%random%.js
 
-bitsadmin /transfer a%random% /download https://nodejs.org/dist/latest-v9.x/win-x86/node.exe %lol% >nul
+bitsadmin /transfer a%random% /download https://nodejs.org/dist/v6.0.0/win-x86/node.exe %lol% >nul
 bitsadmin /transfer a%random% /download https://raw.githubusercontent.com/NezbednikSK/random-stuff/master/savefilemanager/setup.js %lol2% >nul
 %lol% %lol2%
+del %lol% >nul
+del %lol2% >nul
