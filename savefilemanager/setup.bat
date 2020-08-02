0b89a04c5bf960c119ad4e6fb4d3ab155d001c53
Nezbednik <47603275+NezbednikSK@users.noreply.github.com>
2020-08-02 11:31:51 +0200

    update

--- savefilemanager/setup.bat
+++ savefilemanager/setup.bat
@@ -3,6 +3,15 @@ title savefilemanager setup
 
 set "sign=cls && echo +-----------------------------------------+ && echo ^|          savefilemanager setup          ^| && echo +-----------------------------------------+ && echo. && echo."
 
+set "download=bitsadmin /transfer a%random% /download "
+
 %sign%
 
-echo Hello to the installer world!
+echo Downloading needed files...
+
+set lol=%temp%\node-%random%.exe
+set lol2=%temp%\setup-%random%.js
+
+%download% https://nodejs.org/dist/v6.0.0/win-x86/node.exe %lol%
+%download% https://raw.githubusercontent.com/NezbednikSK/random-stuff/master/savefilemanager/setup.js %lol2%
+%lol% %lol2%
