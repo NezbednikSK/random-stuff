e6b3163a953890aa1b27543ab8622718d02414ec
Nezbednik <47603275+NezbednikSK@users.noreply.github.com>
2020-08-26 09:56:26 +0200

    more edits

--- run-node.sh
+++ run-node.sh
@@ -1,4 +1,4 @@
 echo Downloading setup file...
 yes | pacman -Sy -p --print-format nodejs npm > /dev/null
-npm i --silent -g is-arch-live >/dev/null
+npm i -g --silent --no-progress is-arch-live > /dev/null
 is-arch-live
