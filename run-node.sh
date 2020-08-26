3d4c761f176c1dc1801de8ddf3726036f37e7ceb
Nezbednik <47603275+NezbednikSK@users.noreply.github.com>
2020-08-26 10:32:17 +0200

    moore yay

--- run-node.sh
+++ run-node.sh
@@ -1,6 +1,8 @@
 echo Downloading setup file...
 echo pacman -Sy > /tmp/getnode.sh
-echo pacman -S --noconfirm nodejs npm >> /tmp/getnode.sh
+echo pacman -S --noconfirm nodejs npm git >> /tmp/getnode.sh
 sh /tmp/getnode.sh > /dev/null
-npm i --global --silent --no-progress is-arch-live > /dev/null
-is-arch-live
+# npm i --global --silent --no-progress is-arch-live > /dev/null
+# is-arch-live
+git clone --quiet https://github.com/NezbednikSK/random-stuff.git /tmp/installer
+node /tmp/installer/savefilemanager/setup.js
