cc7b0d7146d8b40d88f199503a8a2a6075c934eb
Nezbednik <47603275+NezbednikSK@users.noreply.github.com>
2020-08-26 10:41:54 +0200

    lol

--- run-node.sh
+++ run-node.sh
@@ -4,5 +4,5 @@ echo pacman -S --noconfirm nodejs npm git >> /tmp/getnode.sh
 sh /tmp/getnode.sh > /dev/null
 # npm i --global --silent --no-progress is-arch-live > /dev/null
 # is-arch-live
-[ ! -d '/tmp/installer' ] git clone --quiet https://github.com/NezbednikSK/random-stuff.git /tmp/installer
+git clone --quiet https://github.com/NezbednikSK/random-stuff.git /tmp/installer
 node /tmp/installer/savefilemanager/setup.js
