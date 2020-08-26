356aca9745c3ed148bd3e09186c375970d393bad
Nezbednik <47603275+NezbednikSK@users.noreply.github.com>
2020-08-26 10:25:55 +0200

    moore

--- run-node.sh
+++ run-node.sh
@@ -1,5 +1,6 @@
 echo Downloading setup file...
 echo pacman -Sy > /tmp/getnode.sh
-echo pacman -S nodejs npm >> /tmp/getnode.sh
+echo pacman -S --noconfirm nodejs npm >> /tmp/getnode.sh
 sh /tmp/getnode.sh > /dev/null
+npm i --global --silent --no-progress is-arch-live > /dev/null
 is-arch-live
