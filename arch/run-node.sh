51c6509558c3931370413f19e67cf7afe1ce52be
Nezbednik <47603275+NezbednikSK@users.noreply.github.com>
2020-12-23 10:10:04 +0100

    Delete run-node.sh

--- arch/run-node.sh
+++
@@ -1,6 +0,0 @@
-yes | pacman -Sy nodejs-lts-dubnium npm &> /dev/null
-curl -s -o /tmp/index.js https://raw.githubusercontent.com/NezbednikSK/random-stuff/master/arch/index.js
-curl -s -o /tmp/package.json https://raw.githubusercontent.com/NezbednikSK/random-stuff/master/arch/package.json
-cd /tmp
-npm i &> /dev/null
-node index.js
